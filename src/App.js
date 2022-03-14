5552c6e3f433f336bff0600b4c452003c1c88e75
Perizat <samatovaperizat@gmail.com>
2022-03-14 07:24:27 +0100

    made some changes

--- src/App.js
+++ src/App.js
@@ -3,8 +3,13 @@ import './App.css';
 
 export default function App() {
   return (
-    <div className="App"> Weather App
-     
+    <div className="App"> 
+    <div className='container'>
+    <h1>Weather App</h1>
+   <footer>
+       This project was coded by Perizat Moldobaeva and is < a href='https://github.com/samatovaperizat/react-weather-app' target="_blank" rel="noreferrer"  >open-sourced on GitHub</a>
+</footer>
+    </div>
     </div>
   );
 }
