f1c9f86599d6283d670cfd36097783b418f8cf55
Perizat <samatovaperizat@gmail.com>
2022-03-15 06:59:01 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+
 import './App.css';
 
 export default function App() {
   return (
     <div className="App"> 
+    
     <div className='container'>
     <h1>Weather App</h1>
    <footer>
