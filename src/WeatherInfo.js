712b0068a9caa6564414ef21ca0cdaccab714eb4
Perizat <samatovaperizat@gmail.com>
2022-03-19 14:02:52 +0100

    added Fahrenheit and Celsius

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,8 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
+
 
 export default function WeatherInfo (props) {
     return (
@@ -15,10 +17,7 @@ export default function WeatherInfo (props) {
             <div className="float-left">
             <WeatherIcon code={props.data.icon} />
             </div>
-             
-        <span className="temperature">{Math.round(props.data.temperature)}</span> <span className="unit">°C</span>
-        
-        
+             <WeatherTemperature celsius={props.data.temperature}/>
         </div>
         
         <div className="col-6">
